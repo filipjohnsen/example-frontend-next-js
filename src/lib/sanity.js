5cb6bf8ac7b404ca8fd0ee16312d374518713488
Filip Johnsen <filipjohnsen97@gmail.com>
2020-12-04 23:54:42 +0100

    changed

--- src/lib/sanity.js
+++ src/lib/sanity.js
@@ -4,7 +4,7 @@ const client = sanityClient({
   projectId: "pavz3fta",
   dataset: "production",
   useCdn: true,
-  token: process.env.REACT_APP_SANITY_KEY
+  token: process.env.SANITY_KEY || process.env.REACT_APP_SANITY_KEY
 });
 
 export default client
